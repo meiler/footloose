b4d83829be5a455c2bebfff11f715cc9d30955d7
Pelle Krøgholt <pellekrogholt@gmail.com>
2018-01-24 21:53:56 +0100

    Extract video and audio ~ cross OS friendly

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # footloose
+
+
+
+
+## Extract video and audio
+```
+python dancevideos.py
+```
+
+### Pre-requests
+- python3.5 ~ fire up a virtual env
+- `pip install youtube_dl`
+
+
--- dancevideos.py
+++ dancevideos.py
@@ -1,39 +1,28 @@
-import subprocess
-from os import mkdir
+from __future__ import unicode_literals
+import youtube_dl
+import os
 
-fred_astaire = [
+videos = [
+    # fred_astaire
     'https://www.youtube.com/watch?v=mxPgplMujzQ',
     'https://www.youtube.com/watch?v=CnrbdNjf-aw',
     'https://www.youtube.com/watch?v=yTH9VwACh7o',
     'https://www.youtube.com/watch?v=yuJxYmJlEHY',
     'https://www.youtube.com/watch?v=GKPMk5_gStk',
-]
-
-dancemovies = [
+    # dancemovies
     'https://www.youtube.com/watch?v=j8XGmZ8HDIU',
     'https://www.youtube.com/watch?v=fy0rYUvn7To',
     'https://www.youtube.com/watch?v=pW1hazYKd5o',
     'https://www.youtube.com/watch?v=qdbrIrFxas0',
     'https://www.youtube.com/watch?v=BiAwpYIkRmU',
-]
-
-soul_train = [
+    # soul_train
     'https://www.youtube.com/watch?v=qXbP4JBf8To',
     'https://www.youtube.com/watch?v=qglBm-N-Lnw',
     'https://www.youtube.com/watch?v=lODBVM802H8',
     'https://www.youtube.com/watch?v=0g7KawdsVSQ',
-]
-
-air_guitar = [
-
-]
-
-bollywood = [
+    # air_guitar
     'https://www.youtube.com/watch?v=U_0HSjUpciQ',
-
-]
-
-choreographies = [
+    # choreographies
     'https://www.youtube.com/watch?v=X5qpDrmyO0E',
     'https://www.youtube.com/watch?v=Kl5B6MBAntI',
     'https://www.youtube.com/watch?v=7v5s0wDbEik',
@@ -48,16 +37,12 @@ choreographies = [
     'https://www.youtube.com/watch?v=sUKrTQRNZuw',
     'https://www.youtube.com/watch?v=cqdXgQ5bxxs',
     'https://www.youtube.com/watch?v=h-bsd6FSZDY',
-]
-
-music_videos = [
+    # music_videos
     'https://www.youtube.com/watch?v=Vnoz5uBEWOA',
     'https://www.youtube.com/watch?v=JqYhuwu614Y',
     'https://www.youtube.com/watch?v=wCDIYvFmgW8',
     'https://www.youtube.com/watch?v=9bZkp7q19f0',
-]
-
-internet_shit = [
+    # internet_shit
     'https://www.youtube.com/watch?v=gtakKnP5GcM',
     'https://www.youtube.com/watch?v=ABz2m0olmPg',
     'https://www.youtube.com/watch?v=yMHygHL-obQ',
@@ -68,36 +53,56 @@ internet_shit = [
     'https://www.youtube.com/watch?v=bPn6Dw4f26Q',
 ]
 
+class ScraberLogger(object):
+    def debug(self, msg):
+        pass
 
-def download_video(video_link, output_dir='data/videos/'):
-    vlc_download_cmd = ('/Applications/VLC.app/Contents/MacOS/VLC -I rc '
-                        '--play-and-exit -vvv "{link}" --sout="#transcode'
-                        '{{vcodec=mp4v,acodec=mp4a,samplerate=44100}}'
-                        ':std{{access=file,mux=mp4,dst={output_name}.m4v}}"')
+    def warning(self, msg):
+        pass
 
-    link = video_link
-    output_name = output_dir + video_link.split('=')[-1]
+    def error(self, msg):
+        print(msg)
 
-    subprocess.check_output(
-        vlc_download_cmd.format(
-            link=link, output_name=output_name),
-        shell=True,
-        timeout=120
-    )
+def scraber_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
 
+def mkdir(path):
+    sub_path = os.path.dirname(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
 
-all_vids = {'fred_astaire': fred_astaire,
-            'dancemovies': dancemovies,
-            'soul_train': soul_train,
-            'bollywood': bollywood,
-            'choreographies': choreographies,
-            'music_videos': music_videos,
-            'internet_shit': internet_shit}
+path=os.path.dirname(os.path.abspath(__file__))
+data_path="%s/data" % (path, )
+mkdir(data_path)
+video_path="%s/video/" % (data_path, )
+mkdir(video_path)
+audio_path="%s/audio/" % (data_path, )
+mkdir(audio_path)
 
-for key in all_vids:
-    mkdir('data/videos/%s' % key)
+video_path_opts='{}%(title)s.%(ext)s'.format(video_path)
 
-for name, videos in all_vids.items():
-    for video in videos:
-        print('Downloading %s' % video)
-        download_video(video, 'data/videos/%s/' % name)
+ydl_opts_video = {
+    'f': 'bestvideo[height<=?1080]+bestaudio/best',
+    'recode-video': 'mp4',
+    'logger': ScraberLogger(),
+    'progress_hooks': [scraber_hook],
+    'outtmpl': video_path_opts
+}
+
+with youtube_dl.YoutubeDL(ydl_opts_video) as ydl:
+    ydl.download(videos)
+
+audio_path_opts='{}%(title)s.%(ext)s'.format(audio_path)
+ydl_opts_audio = {
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+    }],
+    'logger': ScraberLogger(),
+    'progress_hooks': [scraber_hook],
+    'outtmpl': audio_path_opts
+}
+with youtube_dl.YoutubeDL(ydl_opts_audio) as ydl:
+    ydl.download(videos)
+
