de45243a260b3da0361615fac045fa42fe74a422
William Kurdahl Larsen <william@Williams-MacBook-Pro.local>
2018-11-13 22:35:33 +0100

    fixed bugs in instrument logic based Mads comments

--- preprocessing/instruments.py
+++ preprocessing/instruments.py
@@ -1,43 +1,45 @@
 # 4_instruments.py
 
 
-def is_drums(track):
-
-
 def is_bass(track):
+    """ Checks if it only plays on pitch at a time and the average pitch is deep.
+    """
     if any(track.sum(axis=1) > 1):
         return False
-    elif track.sum()/len(np.nonzero(track)[0]) > 47:
+    elif np.average(np.nonzero(track)[0]) > 47:
         return False #if the average pitch is below 47, then it may be the bass
     else:
         return True
 
 
 def is_harmony(track):
-    instument_present = [number for number in track.sum(axis=1) if number > 0]
-    harmonies = [number for number in instument_present.sum(axis=1) if number > 1]
-    
-    harmony_percentage = float(len(harmonies)) / float(len(instument_present))
-    
+    """ Checks if it mostly plays several pitch at a time.
+    """
+    instrument_present = [nodes for nodes in track.sum(axis=1) if nodes > 0]
+    harmonies = [nodes for nodes in instrument_present.sum(axis=1) if nodes > 1]
+
+    harmony_percentage = len(harmonies) / len(instrument_present)
+
     if harmony_percentage < 0.9: # often (90 percent) two nodes at the same time        
         return False
     else:
-        return True    
+        return True
     
 
 
 def is_lead(track):
     # the lead is mostly 1 pitch at a time
     # and it is above 47 on average
-    
-    instument_present = [number for number in track.sum(axis=1) if number > 0]
-    harmonies = [number for number in instument_present.sum(axis=1) if number > 1]
-    
-    harmony_percentage = float(len(harmonies)) / float(len(instument_present))    
-    
+    """ Checks if it mostly plays one pitch at a time and the average pitch is high.
+    """
+    instrument_present = [nodes for nodes in track.sum(axis=1) if nodes > 0]
+    harmonies = [nodes for nodes in instrument_present.sum(axis=1) if nodes > 1]
+
+    harmony_percentage = len(harmonies) / len(instrument_present)
+
     if harmony_percentage > 0.1 : # often (90 percent) two nodes at the same time        
         return False
-    elif track.sum()/len(np.nonzero(track)[0]) < 47:
+    elif np.average(np.nonzero(track)[0]) < 47:
         return False
     else:
         return True
