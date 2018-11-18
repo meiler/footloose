8c9da8880b5528ae774b43ccc150a8a3bcf25db2
Mads Eiler Hansen <mads.eilerhansen@leoilab.com>
2018-11-18 17:12:53 +0100

    Refactor encoder

--- preprocessing/midi_encoder.py
+++ preprocessing/midi_encoder.py
@@ -13,147 +13,53 @@ from preprocessing.midi_parser import convert_midi_file
 from mido import Message, MidiFile, MidiTrack
 
 
-convert_midi_file('/Users/william/Projects/footloose/Britney_Spears_-_Baby_One_More_Time.mid')
+def get_off_notes(old_state, new_state):
+    return [pos for (pos, state) in enumerate(new_state) if state == 0 and old_state[pos] == 1]
 
 
-file = mido.MidiFile('/Users/william/Projects/footloose/Britney_Spears_-_Baby_One_More_Time.mid')
+def get_on_notes(old_state, new_state):
+    return [pos for (pos, state) in enumerate(new_state) if state == 1 and old_state[pos] == 0]
 
 
+def turn_off(track, note, time):
+    track.append(Message('note_on', note=note, velocity=0, time=time))
 
 
-from mido import Message
-msg = Message('note_on', note=60)
-msg
+def turn_on(track, note, time):
+    track.append(Message('note_on', note=note, velocity=100, time=time))
 
 
+def get_track_from_array(array_track, tick_size=15, instrument=1):
+    mid_track = mido.MidiTrack()
+    state = np.zeros(128)
+    last_event = 0
+
+    for time in range(len(array_track)):
+        new_state = array_track[time, :]
+
+        on_notes = get_on_notes(state, new_state)
+        off_notes = get_off_notes(state, new_state)
+
+        for note in on_notes:
+            delta_time = (time - last_event) * tick_size
+            turn_on(mid_track, note, delta_time)
+            last_event = time
+
+        for note in off_notes:
+            delta_time = (time - last_event) * tick_size
+            turn_off(mid_track, note, delta_time)
+            last_event = time
+
+        state = new_state
+
+    return mid_track
 
 
-
-
-
-from mido import Message, MidiFile, MidiTrack
-
-mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
-
-
-track.append(Message('program_change', program=12, time=0))
-track.append(Message('note_on', note=64, velocity=64, time=32))
-track.append(Message('note_off', note=64, velocity=127, time=32))
-
-mid.save('new_song.mid')
-
-
-tic_size = get_tick_size('/Users/william/Projects/footloose/Britney_Spears_-_Baby_One_More_Time.mid')
-
-tick_size = 15
-program_num = 12
-
-array_tracks = convert_midi_file('/Users/william/Projects/footloose/Britney_Spears_-_Baby_One_More_Time.mid')
-
-def add_track_from_array(instrument, tick_size, track_input):
-    track.append(Message('program_change', program=instrument, time=0))
-    for note_num in range(0,128):
-        last_time = 0
-        hold_note = False
-        for timestep in range(len(track_input)):
-            if track_input[timestep][note_num] == 1:
-                if hold_note == False:
-                    #start node
-                    time = timestep*tick_size-last_time
-                    last_time = timestep*tick_size
-                    hold_note = True
-                    track.append(Message('note_on', note=note_num, velocity=100, time=time))
-            else:
-                if hold_note == True:
-                    #end node
-                    time = timestep*tick_size-last_time
-                    last_time = timestep*tick_size
-                    hold_note = False
-                    #adds a message with 0 velocity equivalent to note_off
-                    track.append(Message('note_on', note=note_num, velocity=0, time=time))
-
-
-
-
-def convert_tensor_to_midi(array_tracks,filename):
-    instrument = 1 #all instruments are piano
-    tick_size = 15 # sets the tempo
-    
+def convert_tensor_to_midi(array_tracks, filename):
     mid = MidiFile()
-    
-    for i in range(0,len(array_tracks)-1):
-        track = MidiTrack()
-        mid.tracks.append(track)
-        
-        track_input = array_tracks[i]
-        add_track_from_array(instrument, tick_size, track_input)
+
+    for channel in array_tracks:
+        mid_track = get_track_from_array(array_tracks[channel])
+        mid.tracks.append(mid_track)
+
     mid.save(filename)
-
-
-#get_tick_size('/Users/william/Projects/footloose/Britney_Spears_-_Baby_One_More_Time.mid')
-
-
-
-
-array_tracks = convert_midi_file('/Users/william/Projects/footloose/Britney_Spears_-_Baby_One_More_Time.mid')
-filename = '/Users/william/Projects/footloose/Britney_Spears_-_recontructed3.mid'
-
-convert_tensor_to_midi(array_tracks,filename)
-
-
-
-
-for timestep in range(conv[0][0]):
-    print(timestep)
-
-
-
-
-
-from mido import Message, MidiFile, MidiTrack
-
-mid = MidiFile()
-
-track = MidiTrack()
-mid.tracks.append(track)
-
-
-track.append(Message('program_change', program=1, time=0))
-track.append(Message('note_on', note=64, velocity=100, time=0))
-track.append(Message('note_on', note=64, velocity=0, time=15))
-
-track = MidiTrack()
-mid.tracks.append(track)
-
-track.append(Message('note_on', note=1, velocity=100, time=0))
-track.append(Message('note_on', note=1, velocity=0, time=15))
-
-track = MidiTrack()
-mid.tracks.append(track)
-
-track.append(Message('note_on', note=2, velocity=100, time=0))
-track.append(Message('note_on', note=2, velocity=0, time=11))
-track.append(Message('note_on', note=2, velocity=100, time=1))
-track.append(Message('note_on', note=2, velocity=0, time=1))
-track.append(Message('note_on', note=2, velocity=100, time=1))
-track.append(Message('note_on', note=2, velocity=0, time=1))
-
-mid.save('new_song3.mid')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
