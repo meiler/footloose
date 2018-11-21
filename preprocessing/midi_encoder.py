a82b00f5df0cc0e15b2a78e084d60c551f1d05e7
Simon Felding <simonfelding@gmail.com>
2018-11-21 17:08:25 +0100

    added general & instrument-specific metadata

--- preprocessing/midi_encoder.py
+++ preprocessing/midi_encoder.py
@@ -8,9 +8,8 @@ The input tensor should contain: time (midi ticks) x pitch (127) x instruments
 """
 import numpy as np
 import mido
-#from .instruments import is_bass, is_harmony, is_lead
-from preprocessing.midi_parser import convert_midi_file
-from mido import Message, MidiFile, MidiTrack
+from preprocessing.instruments import is_bass, is_harmony, is_lead, is_drums
+from mido import Message, MidiFile, MetaMessage
 import scipy
 
 
@@ -64,26 +63,31 @@ def convert_tensor_to_midi(array_tracks, suffix, instrument = 1):
     title = "one more time"
 
     # append meta track 0
-    mid.tracks[0].append(Message('meta message', 'track_name', artist, 'time=0'))
-    mid.tracks[0].append(Message('meta message', 'track_name', title, 'time=0'))
-    mid.tracks[0].append(Message('meta message', 'time_signature', 'numerator=4', 'denominator=4', 'clocks_per_click=24', 'notated_32nd_notes_per_beat=8', 'time=0'))
-    mid.tracks[0].append(Message('meta message', 'set_tempo', 'tempo=' + str(tempo), 'time=0'))
+    mid.tracks[0].append(MetaMessage('midi_port', 'port=0', 'time=0'))
+    mid.tracks[0].append(MetaMessage('track_name', artist, 'time=0'))
+    mid.tracks[0].append(MetaMessage('track_name', title, 'time=0'))
+    mid.tracks[0].append(MetaMessage('time_signature', 'numerator=4', 'denominator=4', 'clocks_per_click=24', 'notated_32nd_notes_per_beat=8', 'time=0'))
+    mid.tracks[0].append(MetaMessage('set_tempo', 'tempo=' + str(tempo), 'time=0'))
 
     for channel in array_tracks:
         mid_track = get_track_from_array(array_tracks[channel])
         track = 1
-        mid.tracks[track].append(Message('meta message', 'midi_port', 'port=0', 'time=0')) # track header
+        mid.tracks[track].append(MetaMessage('midi_port', 'port=0', 'time=0')) # track header
 
         if instrument == 1:
             if is_lead(mid_track):
-                mid.tracks[track].append(Message('program_change', channel=channel, program=74, time=0))  # 74 is flute
+                # 57 is trumpet!
+                mid.tracks[track].append(Message('program_change', channel=channel, program=57, time=0))
             elif is_bass(mid_track):
-                mid.tracks[track].append(Message('program_change', channel=channel, program=34, time=0))  # 34 is bass
+                # 34 is bass
+                mid.tracks[track].append(Message('program_change', channel=channel, program=34, time=0))
             elif is_harmony(mid_track):
-                mid.tracks[track].append(Message('program_change', channel=channel, program=1, time=0))  # 1 is piano
+                # 1 is piano
+                mid.tracks[track].append(Message('program_change', channel=channel, program=1, time=0))
             elif is_drums(mid_track):
-                mid.tracks[track].append(Message('control_change', channel=9, control=10, value=64, time=0)) # channel 9 is always drums
-                # how britney.mid sets the drums but I think it's unnecessary. IIRC control=10 is pan adjustment, 64 centers it.
+                # channel 9 is always drums.
+                mid.tracks[track].append(Message('control_change', channel=9, control=10, value=64, time=0))
+                # how britney.mid sets drums. I think it's unnecessary. iirc ctrl=10 is pan adjustment, val=64 centers.
 
         mid.tracks[track].append(mid_track)
         track = track + 1
